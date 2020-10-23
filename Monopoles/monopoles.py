5c6fdbc32ca013c232267d3aa91cdd1abe958661
Mi Yon Kim <miyon@pdx.edu>
2020-10-23 14:40:30 -0700

    Implement new solver for Monopoles puzzle

--- Monopoles/monopoles.py
+++ Monopoles/monopoles.py
@@ -5,104 +5,81 @@
 import sys
 
 
+
 class Monopole:
 
     def __init__(self, num_monopoles, num_rooms):
-        self.monopoles = []
+        self.pvars = set()
         for i in range(int(num_monopoles)):
-            self.monopoles.append(i+1)
+            self.pvars.add(i+1)
+
         self.rooms = [None] * int(num_rooms)
         for i in range(int(num_rooms)):
             self.rooms[i] = list()
+
         self.rooms_sum = [None] * int(num_rooms)
         for i in range(int(num_rooms)):
             self.rooms_sum[i] = set()
-        self.cur_room_num = int(0)
+
         self.num_rooms = int(num_rooms)
-        self.num_fail = 0
 
-    def show(self):
-        print(self.monopoles)
-        print(self.rooms)
-        print(self.rooms_sum)
+    def show(self, vals=dict()):
+        print(vals)
 
-    def solve(self):
-        # base case
-        if len(self.monopoles) == 0:
-            return True
+    def solve(self, pvars=None, vals=None):
+        # New solution attempt. Can't put these values
+        # inline because bad things happen with Python's
+        # evaluator.
+        if pvars is None:
+            pvars = list(self.pvars)
+        if vals is None:
+            vals = dict()
 
-        # base case
-        if self.num_fail == self.num_rooms:
-            return None
-
-        self.num_fail = 0
-        # Recursive case: try to put monopole into the room
-        peek = self.monopoles[0]
-        # Iterate room from 0 through n-1
-        for i in range(self.num_rooms):
-            # if the monopole can append to current room
-            if peek not in self.rooms_sum[i]:
-                m = peek
-                n_monopoles = len(self.rooms[i])
-                undo = False
-                temp_set = set()
-                # Append the sum of monopoles to rooms_sum
-                if n_monopoles >= 1:
-                    rooms_sum_wo_cur = list(self.rooms_sum)
-                    rooms_sum_wo_cur.remove(self.rooms_sum[i])
-                    for j in self.rooms[i]:
-                        temp_set.add(j+m)
-                    # iterate temp_set check if all of the rooms_sum has element of temp_set
-                    # if one of the element meets the condition, undo
-                    for k in temp_set:
-                        cnt = 0
-                        for r_sum in rooms_sum_wo_cur:
-                            if k in r_sum and m not in r_sum:
-                                cnt += 1
-                        if len(rooms_sum_wo_cur) == cnt:
-                            undo = True
-
-                            break
-                if not undo:
-                    self.monopoles.pop(0)
-                    self.rooms_sum[i] = self.rooms_sum[i].union(temp_set)
-                    self.rooms[i].append(m)
-                    break
-            else:
-                self.num_fail += 1
-
-        sol = self.solve()
-        if sol:
-            return sol
-
-    def solve2(self):
-        # base case
-        if len(self.monopoles) == 0:
-            return True
-
-        # Recursive case:
-        peek = self.monopoles[0]
-        if peek not in self.rooms_sum[self.cur_room_num]:
-            m = self.monopoles.pop(0)
-            n_monopoles = len(self.rooms[self.cur_room_num])
-            if n_monopoles >= 1:
-                for i in self.rooms[self.cur_room_num]:
-                    self.rooms_sum[self.cur_room_num].add(i + m)
-            self.rooms[self.cur_room_num].append(m)
-            # reset the number of fail to append
-            self.num_fail = 0
-        else:
-            if self.cur_room_num != self.num_rooms - 1:
-                self.cur_room_num += 1
-            else:
-                self.cur_room_num = 0
-            self.num_fail += 1
-            if self.num_fail == self.num_rooms:
+        # Return True iff no constraint violations
+        def ok():
+            # Return the value of the variable,
+            # or None if unvalued.
+            def value(var):
+                if var in vals:
+                    return vals[var]
                 return None
 
-        sol = self.solve2()
-        if sol:
-            return sol
+            if len(vals.values()) < 3:
+                return True
+            for r_num in range(self.num_rooms):
+                room = [monopole for monopole, r_n in vals.items() if r_n == r_num]
+
+                if len(room) <= 2:
+                    continue
+                for m1 in range(0, len(room)-1):
+                    for m2 in range(m1+1, len(room)):
+                        sum = room[m1] + room[m2]
+                        if sum in room:
+                            return False
+            return True
+
+        # Base case: check for failure.
+        if not ok():
+            return None
+
+        # Base case: check for solution.
+        if not pvars:
+            return vals
+
+        # Recursive case:
+        v = pvars.pop(0)
+        assert v not in vals
+        for val in range(self.num_rooms):
+            vals[v] = val
+            soln = self.solve(pvars=pvars, vals=vals)
+            if soln:
+                return soln
+
+        # No solution found. Undo the current assignment and
+        # return failure.
+        del vals[v]
+        pvars.append(v)
+        return None
 
 
 monopole = Monopole(sys.argv[1], sys.argv[2])
@@ -111,5 +88,5 @@ vals = monopole.solve()
 if vals is None:
     print("unsat")
 else:
-    monopole.show()
+    monopole.show(vals)
 
