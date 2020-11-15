47555e878f0e07befaf049150539af0f7fc334cb
Mi Yon Kim <miyon@pdx.edu>
2020-11-14 23:49:43 -0800

    Add documentation & implement unmonosat

---
+++ Monopoles via SAT/unmonosat.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+# Mi Yon Kim
+
+# Read DIMACS format SAT solver output
+# http://www.satcompetition.org/2009/format-benchmarks2009.html
+# to solve a monopole instance.
+
+import sys
+from sys import stdin, stderr
+
+num_of_monopoles, num_of_rooms = int(sys.argv[1]), int(sys.argv[2])
+
+monopole_table = [[None]*num_of_monopoles for _ in range(num_of_rooms)]
+
+def unlit(b):
+    b -= 1
+    m = b % num_of_monopoles + 1
+    n = b // num_of_monopoles
+    return (m, n)
+
+header = next(stdin).strip()
+if header == "s SATISFIABLE":
+    # picosat
+    lines = [l[1:] for l in stdin.readlines() if l and l[0] == 'v']
+    solution = [int(b) for l in lines for b in l.split() if int(b) > 0]
+elif header == "SAT":
+    # minisat
+    solution = [int(b) for b in next(stdin).split() if int(b) > 0]
+else:
+    print("unknown solution format", file=stderr)
+    exit(-1)
+
+for n in solution:
+    (r, c) = unlit(n)
+    assert monopole_table[r][c] == None
+    monopole_table[r][c] = r
+
+for r in range(num_of_monopoles):
+    for c in range(num_of_rooms):
+        print(monopole_table[r][c], end="")
+    print()
+
